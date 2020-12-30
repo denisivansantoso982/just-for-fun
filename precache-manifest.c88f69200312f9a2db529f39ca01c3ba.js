e7598427e5faea17cb6a2282ce076c4e659ae2d0
Denis Ivan Santoso <denisivansantoso@gmail.com>
2020-12-30 17:25:12 +0700

    Updates

---
+++ precache-manifest.c88f69200312f9a2db529f39ca01c3ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59057e06c187be0ade447f72943e7198",
+    "url": "/just-for-fun/index.html"
+  },
+  {
+    "revision": "f946a85e2e3ce2a36110",
+    "url": "/just-for-fun/static/css/main.6e76002b.chunk.css"
+  },
+  {
+    "revision": "2c5e01b858bcc39ff1f3",
+    "url": "/just-for-fun/static/js/2.1af4ff13.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/just-for-fun/static/js/2.1af4ff13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f946a85e2e3ce2a36110",
+    "url": "/just-for-fun/static/js/main.2c7d1b87.chunk.js"
+  },
+  {
+    "revision": "2f8c8cbb9f869288ea12",
+    "url": "/just-for-fun/static/js/runtime-main.43071ade.js"
+  }
+]);
