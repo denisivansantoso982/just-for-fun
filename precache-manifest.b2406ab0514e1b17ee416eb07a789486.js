04f4817af2c56718c065fcd4f1d16356d19d582b
Denis Ivan Santoso <denisivansantoso@gmail.com>
2020-12-30 17:31:24 +0700

    Updates

--- precache-manifest.b2406ab0514e1b17ee416eb07a789486.js
+++ precache-manifest.b2406ab0514e1b17ee416eb07a789486.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d0bf3c4a5678ea8bf0a0bf1da0790528",
+    "revision": "44a154ce45d91da9aff5336815bf1971",
     "url": "/just-for-fun/index.html"
   },
   {
-    "revision": "e796caf2cc711538dd61",
+    "revision": "d2d76b056b283c1faf63",
     "url": "/just-for-fun/static/css/main.6e76002b.chunk.css"
   },
   {
-    "revision": "2c5e01b858bcc39ff1f3",
-    "url": "/just-for-fun/static/js/2.1af4ff13.chunk.js"
+    "revision": "64f9c736a9b1288e66aa",
+    "url": "/just-for-fun/static/js/2.3ac8f6ec.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/just-for-fun/static/js/2.1af4ff13.chunk.js.LICENSE.txt"
+    "url": "/just-for-fun/static/js/2.3ac8f6ec.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e796caf2cc711538dd61",
-    "url": "/just-for-fun/static/js/main.bab2a669.chunk.js"
+    "revision": "d2d76b056b283c1faf63",
+    "url": "/just-for-fun/static/js/main.78f1e945.chunk.js"
   },
   {
     "revision": "2f8c8cbb9f869288ea12",
